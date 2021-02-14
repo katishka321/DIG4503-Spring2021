71a0e02c2636dff430ff12f00540582087d1da26
Katishka321 <70786175+katishka321@users.noreply.github.com>
2021-02-13 20:24:14 -0500

    lab4

---
+++ lab4/src/App.js
@@ -0,0 +1,12 @@
+// Importing React and everthing it contains.
+import React from 'react';
+//Importing Market and everything it contains.
+import Market from './Components/Market';
+
+function App () {
+  return (
+    <Market/>
+  )
+}
+
+export default App;
